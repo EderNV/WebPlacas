989ff696632a5a9f4baca51ae78eeb684a6adcce
Eder Naranjo Vargas <Eder Naranjo Vargas>
2016-10-24 17:24:49 -0600

    Interface web Placas

---
+++ subir.php
@@ -0,0 +1,16 @@
+<?php
+mysql_connect("localhost", "root", "") or die(mysql_error()) ;
+mysql_select_db("bd_banners") or die(mysql_error()) ;
+$imagen1= $_FILES["imagen"]["name"];
+$ruta=$_FILES["imagen"]["tmp_name"];
+$destino="imagenes/".$imagen1;
+ $fp     = fopen($ruta, 'r+b');
+$data = fread($fp, filesize($ruta));
+fclose($fp);
+
+                //escapar los caracteres
+                $data = mysql_escape_string($data);
+copy($ruta, $destino);
+mysql_query("INSERT INTO imagenes (imagen, tipo_imagen) VALUES ('$data', '$destino')") ;
+header("location: dashboard.php");
+?>
